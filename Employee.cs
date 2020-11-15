9ee4b47a252180a8b35d2a689041efe2cdfe43a9
authentictorch25 <64377971+authentictorch25@users.noreply.github.com>
2020-11-15 14:36:25 +0530

    [Akash]Added test case for UC1

---
+++ Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestApi
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string job { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public string region { get; set; }
+        public long phoneNumber { get; set; }
+        public string email { get; set; }
+
+        public long salary { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RestApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rest API");
+        }
+    }
+}
